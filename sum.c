41aba79accd476c1f165c089ccb55804e4a21726
Hinafemin <118240617+Hinafemin@users.noreply.github.com>
2022-11-15 12:16:18 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int first,second,sum;
+ printf("Enter first value : ");
+ scanf("%d",&first);
+ printf("Enter second value : ");
+ scanf("%d",&second);
+ sum=first+second;
+ printf("Sum = %d\n",sum);
+}
